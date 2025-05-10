87f4adc01498fb38410fee7b06931cae528cb6e5
TomPGrid <tprusalowicz@griddynamics.com>
2025-05-10 17:17:45 +0200

    change terraform code

--- infra-aws/main.tf
+++ infra-aws/main.tf
@@ -1,19 +1,23 @@
+provider "aws" {
+  region = var.region
+}
+
 module "network" {
   source = "./modules/network"
 }
 
 module "policies" {
-  source                   = "./modules/policies"
-  instance_role_name       = "ecs-host-role"
-  instance_profile_name    = "ecs-host-profile"
-  task_execution_role_name = "ecs-task-exec-role"
+  source                    = "./modules/policies"
+  instance_role_name        = "ecs-host-role"
+  instance_profile_name     = "ecs-host-profile"
+  task_execution_role_name  = "ecs-task-exec-role"
 }
 
 module "compute" {
   source                = "./modules/compute"
   cluster_name          = "petclinic-cluster"
   instance_type         = var.instance_type
-  instance_profile_name = module.iam.instance_profile_name
+  instance_profile_name = module.policies.instance_profile_name
   subnet_ids            = [ module.network.subnet_id ]
   instance_sg_ids       = module.network.sg_ids
   min_size              = var.min_size
