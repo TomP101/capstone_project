9a6d251a701578f09a05bd4dce524f685cdc6946
TomPGrid <tprusalowicz@griddynamics.com>
2025-05-10 17:53:28 +0200

    fix outputt.tf in root

--- infra-aws/output.tf
+++ infra-aws/output.tf
@@ -1,9 +1,1 @@
-output "cluster_id" {
-  description = "ECS cluster ARN"
-  value       = module.compute.cluster_id
-}
-
-output "asg_name" {
-  description = "Name of the ECS AutoScalingGroup"
-  value       = module.compute.asg_name
-}
+ 
