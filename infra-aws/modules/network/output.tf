c0ed6672354a5ba08973afea15b3794a0d5bfb98
TomPGrid <tprusalowicz@griddynamics.com>
2025-05-12 08:51:20 +0200

    change output of network module

--- infra-aws/modules/network/output.tf
+++ infra-aws/modules/network/output.tf
@@ -1,23 +1,27 @@
-output "subnet_id" {
-  description = "The ID of the subnet"
-  value       = aws_subnet.subnet_eu_north1a.id
+output "subnet_ids" {
+  description = "Lista subnetów (eu-north-1a i eu-north-1b) dla zasobów"
+  value       = [
+    aws_subnet.subnet_eu_north1a.id,
+    aws_subnet.subnet_eu_north1b.id,
+  ]
 }
 
 output "sg_ids" {
-  description = "The IDs of the security groups"
-  value       = [aws_security_group.sg1.id]
+  description = "Lista Security Groups dla instancji i ALB"
+  value       = [ aws_security_group.sg1.id ]
 }
 
-output "tg_arn" {
-  description = "The ARN of the target group"
-  value       = aws_lb_target_group.tg_ec2.arn
+output "alb_listener_arn" {
+  description = "ARN istniejącego listenera ALB (port 80)"
+  value       = aws_lb_listener.listener_forward_all.arn
 }
+
 output "lb_arn" {
-  description = "The ARN of the load balancer"
-  value       = aws_lb.application-lb.id
+  description = "ARN ALB"
+  value       = aws_lb.application-lb.arn
 }
 
 output "lb_dns_name" {
-  description = "The DNS name of the load balancer"
+  description = "DNS name ALB"
   value       = aws_lb.application-lb.dns_name
 }
